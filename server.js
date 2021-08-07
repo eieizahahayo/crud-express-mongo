a76848f9e56db219d715ca0d24be85723cf53799
NIPA-LT0053 <nipa-lt0053@NIPA-LT0053s-MacBook-Air.local>
2021-08-08 03:28:48 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log("May Node be with you, listening on 3000");
+});
+
+// We normally abbreviate `request` to `req` and `response` to `res`.
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
+app.get("/", (req, res) => {
+  //   res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
+  console.log(__dirname);
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+});
+
+app.post("/quotes", (req, res) => {
+  console.log(req.body);
+});
