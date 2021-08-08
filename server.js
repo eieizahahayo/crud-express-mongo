cb372a0aaf2202a81848af8cf9c789c669ac1552
NIPA-LT0053 <nipa-lt0053@NIPA-LT0053s-MacBook-Air.local>
2021-08-09 02:40:56 +0700

    complete tutorial

--- server.js
+++ server.js
@@ -5,25 +5,74 @@ const MongoClient = require("mongodb").MongoClient;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+
+const connectionString =
+  "mongodb+srv://card:Mongo741852963@cluster0.yjwve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("card-database");
+    const quotesCollection = db.collection("quotes");
+
+    app.get("/", (req, res) => {
+      db.collection("quotes")
+        .find()
+        .toArray()
+        .then((results) => {
+          res.render("index.ejs", { quotes: results });
+        })
+        .catch(/* ... */);
+    });
+
+    app.put("/quotes", (req, res) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: "Yoda" },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+        .then((result) => {
+          res.json("Success");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.delete("/quotes", (req, res) => {
+      quotesCollection
+        .deleteOne({ name: req.body.name })
+        .then((result) => {
+          if (result.deletedCount === 0) {
+            return res.json("No quote to delete");
+          }
+          res.json(`Deleted Darth Vadar's quote`);
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
+    });
+  })
+  .catch((error) => console.error(error));
+
 app.listen(3000, function () {
   console.log("May Node be with you, listening on 3000");
 });
-
-// We normally abbreviate `request` to `req` and `response` to `res`.
-// app.get('/', function (req, res) {
-//     res.send('Hello World')
-// })
-
-app.get("/", (req, res) => {
-  //   res.send("Hello World");
-  res.sendFile(__dirname + "/index.html");
-  console.log(__dirname);
-  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-});
-
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
-
-MongoClient.connect("mongodb-connection-string", (err, client) => {});
